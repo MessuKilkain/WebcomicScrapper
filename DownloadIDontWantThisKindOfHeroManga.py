26ee6f4073a983e8262249f4250c3b1c7af620bb
MessuKilkain <magniez.indiana@gmail.com>
2017-02-18 12:36:42 +0100

    Transform WebcomicScrapper_IDontWantThisKindOfHero into a subclass of WebcomicScrapper

--- DownloadIDontWantThisKindOfHeroManga.py
+++ DownloadIDontWantThisKindOfHeroManga.py
@@ -10,135 +10,19 @@ import requests
 import urllib.parse
 import datetime
 
+from WebcomicScrapper import WebcomicScrapper
+
 # imageFilesDestinationFolder = cleanStringForFolderName('I_Don_t_Want_This_Kind_of_Hero_-_Manga')
 imageFilesDestinationFolder = 'I_Don_t_Want_This_Kind_of_Hero_-_Manga'
 pageCountLimit = 1000
 
 firstCommicUrl = 'http://mangafox.me/manga/i_don_t_want_this_kind_of_hero/c142/1.html'
 
-class WebcomicScrapper_IDontWantThisKindOfHero(object):
+class WebcomicScrapper_IDontWantThisKindOfHero(WebcomicScrapper):
 
 	def __init__(self):
-		self._validCharsForFolderName = "-_.()%s%s" % (string.ascii_letters, string.digits)
-		self._startComicUrl = ''
-		self._imageFilesDestinationFolder = ''
-		self._pageCountLimit = 1
+		WebcomicScrapper.__init__(self, startComicUrl='http://mangafox.me/manga/i_don_t_want_this_kind_of_hero/c000/1.html', imageFilesDestinationFolder='I_Don_t_Want_This_Kind_of_Hero_-_Manga', pageCountLimit=50 )
 
-	@property
-	def validCharsForFolderName(self):
-		"""List of characters accepted for folder name."""
-		return self._validCharsForFolderName
-	
-	@property
-	def startComicUrl(self):
-		return self._startComicUrl
-	@startComicUrl.setter
-	def startComicUrl(self,value):
-		if not isinstance(value, str):
-			raise ValueError("startComicUrl is expected to be a string")
-		else:
-			self._startComicUrl = value
-		return
-	
-	@property
-	def imageFilesDestinationFolder(self):
-		return self._imageFilesDestinationFolder
-	@imageFilesDestinationFolder.setter
-	def imageFilesDestinationFolder(self,value):
-		if not isinstance(value, str):
-			raise ValueError("imageFilesDestinationFolder is expected to be a string")
-		else:
-			self._imageFilesDestinationFolder = self.cleanStringForFolderName(value)
-		return
-	
-	@property
-	def pageCountLimit(self):
-		return self._pageCountLimit
-	@pageCountLimit.setter
-	def pageCountLimit(self,value):
-		if ( not isinstance(value, int) ) or value < -1 or value == 0:
-			raise ValueError("pageCountLimit is expected to be a positive int or -1 for no limit")
-		else:
-			self._pageCountLimit = value
-		return
-	
-	def is_integer(self,s):
-		try:
-			int(s)
-			return True
-		except ValueError:
-			return False
-
-	def print_FileAndSysout(self,*objects, end='\n'):
-		print(*objects)
-		if __file__:
-			with open(os.path.basename(__file__)+'.log', 'a') as f:
-				print(*objects, file=f)
-	def logInfo(self,*objects, end='\n'):
-		self.print_FileAndSysout(*objects, end)
-		return
-	def logWarn(self,*objects, end='\n'):
-		self.print_FileAndSysout(*objects, end)
-		return
-	def logDebug(self,*objects, end='\n'):
-		# self.print_FileAndSysout(*objects, end)
-		return
-	
-	def cleanStringForFolderName(self,stringToClean):
-		if not isinstance(stringToClean, str):
-			raise ValueError("stringToClean is expected to be a string")
-			return
-		temp = ''
-		for c in stringToClean:
-			if c in self.validCharsForFolderName:
-				temp += str(c)
-			else :
-				temp += '_'
-		return temp
-
-	def start(self, shouldPauseAtEnd=True):
-		self.logInfo("\nStar scrapping :",str(datetime.datetime.now()),'\n')
-
-		nextUrl = self.startComicUrl
-		pageCount = 0
-		while ( self.pageCountLimit == -1 or pageCount < self.pageCountLimit) and nextUrl:
-			self.logInfo('#'+str(pageCount),'Next Url :',nextUrl)
-			r = requests.get(nextUrl)
-			if r.status_code == 200 :
-				soup = BeautifulSoup(r.text,'html.parser')
-				
-				(nextUrl,imageFileName,imgSrc) = self.getValuesFromPage( soup, r )
-				
-				if not imgSrc:
-					self.logWarn('imgSrc is incorrect')
-				elif not imageFileName :
-					self.logWarn('imageFileName is incorrect')
-				else:
-					if os.path.isfile( imageFileName ):
-						self.logInfo('\tFile '+imageFileName+' already exists.')
-					else:
-						imageRequest = requests.get(imgSrc)
-						if imageRequest.status_code != 200:
-							self.logWarn('\tImage request failed :',r)
-						else:
-							with open(imageFileName, 'wb') as f:
-								f.write(imageRequest.content)
-								self.logInfo('\tImage saved as '+imageFileName)
-				
-				if nextUrl :
-					urlParsed = urllib.parse.urlparse(nextUrl)
-					if not( urlParsed.scheme and urlParsed.netloc and urlParsed.path ):
-						nextUrl = ''
-				if nextUrl:
-					pageCount += 1
-
-		self.logInfo('Page Count :',pageCount)
-		
-		if shouldPauseAtEnd:
-			os.system("pause")
-		
-		return pageCount
-	
 	# return (nextUrl,imageFileName,imgSrc)
 	def getValuesFromPage(self,soup,request):
 		imgSrc = ''
@@ -215,8 +99,6 @@ class WebcomicScrapper_IDontWantThisKindOfHero(object):
 scrapper = WebcomicScrapper_IDontWantThisKindOfHero()
 
 scrapper.startComicUrl = firstCommicUrl
-scrapper.pageCountLimit = pageCountLimit
-scrapper.imageFilesDestinationFolder = imageFilesDestinationFolder
 
 scrapper.start(True)
 
